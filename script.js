f4b4f11d3ff4023d592b72efd3944503c79e1ada
nithish-org <suri20272027@gmail.com>
2024-09-19 20:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function calculateFLAMES() {
 
 function getFLAMESResult(name1, name2) {
     let count = getCommonCharacterCount(name1, name2);
-    const flames = ['FRIENDS LIKE "DEVA & SURIYA"', 'LOVE LIKE "VIJAY & JULIET"', 'AFFECTION "TYPE OF LOVE AND KINDNESS"', 'MARRIAGE "RIP"', 'ENEMIES LIKE "TOM & JERRY"', 'SISTER LIKE "UNIVERSE"'];
+    const flames = ['FRIENDS LIKE "DEVA & SURIYA"', 'LOVE LIKE "VIJAY & GOWSI"', 'AFFECTION "TYPE OF LOVE AND KINDNESS"', 'MARRIAGE "RIP"', 'ENEMIES LIKE "TOM & JERRY"', 'SISTER LIKE "UNIVERSE"'];
 
     while (flames.length > 1) {
         const index = (count - 1) % flames.length;
